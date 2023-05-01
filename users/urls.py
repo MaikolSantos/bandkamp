09e3ce56c27fda0e5b258f7948656a4c8eab9d7e
Maikol Santos <maikol.lourencon@gmail.com>
2023-05-01 20:11:27 -0300

    feat: Tarefa 3 | Sobrescrita com Generic View

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
 from rest_framework_simplejwt import views as jwt_views
+
+from . import views
 
 urlpatterns = [
     path("users/", views.UserView.as_view()),
